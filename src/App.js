e757747fff19d897ee57c89f1189e40f89a1ae6d
sowmiya1302 <you@example.com>
2024-10-18 17:53:37 +0100

    movie engine ready

---
+++ src/App.js
@@ -0,0 +1,52 @@
+// App.js
+
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import MovieCard from './components/MovieCard';
+
+const API_URL = 'https://omdbapi.com?apikey=fe2f6c44';
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState([]);
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    setMovies(data.Search);
+  }
+  useEffect(() => {
+    searchMovies('SpiderMan');
+  }, []);
+  return (
+    <div className="app">
+      <h1>Prime Cinema Spot</h1>
+
+      <div className="search">
+        <input
+          placeholder="Search for Movies"
+          value={searchTerm}
+          onChange={(e) => { setSearchTerm(e.target.value) }}
+        />
+        <img
+          src=
+          "https://media.geeksforgeeks.org/wp-content/uploads/20230626112934/search.png"
+          alt="search icon"
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
+
+      {
+        movies?.length > 0
+          ? (<div className="container">
+            {movies.map((movie) => (
+              <MovieCard movie={movie} />
+            ))}
+          </div>) : (
+            <div className="empty">
+              <h2>No Movies found</h2>
+            </div>
+          )
+      }
+    </div>
+  );
+}
+export default App;
